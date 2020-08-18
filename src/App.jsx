5056ae6b53ff2a045e7750432b6693cfb52768b0
Alexey <apill@ukr.net>
2020-08-18 21:46:44 +0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from './header/Header';
+// import Week from './main/Week';
+
+
+const App = () => {
+    
+  return (
+    <div className='App'>
+    <Header />
+      
+    </div>  
+  );
+};
+
+export default App;
+
+
+      {/* <Week /> */}
+
+
+ 
---
+++ src/header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './header.scss';
+
+const Header = () => {
+
+  return (
+    <header className='header'>
+      <div className='header-nav'>
+        
+      </div>
+    </header>
+  )
+}
+
+export default Header;
